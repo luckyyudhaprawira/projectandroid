684276e90c1622afb6afcad84faf2d4c6b5c3f5d
luckyyudhaprawira <luckyyudha15@gmail.com>
2020-06-04 00:06:00 +0800

    Membuat Menu.java
    1. menambahkan beberapa variabel string untuk class menu dan data dari adapter
    2. private string dari public class menu di isi oleh beberapa data terlampir

---
+++ MenuMakanan/app/src/main/java/com/example/menumakanan/Menu.java
@@ -0,0 +1,14 @@
+package com.example.menumakanan;
+
+public class Menu {
+    private String nama;
+    private String harga;
+    private String gambar;
+    private String detail;
+
+    public Menu(String dataNama, String dataHarga, String dataGambar, String dataDetail) {
+        nama = dataNama;
+        harga = dataHarga;
+        gambar = dataGambar;
+        detail = dataDetail;
+    }
