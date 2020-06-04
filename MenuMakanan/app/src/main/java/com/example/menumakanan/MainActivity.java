428b4ae9b026e7c9dc6b8b162067a0b1ed1b76a8
luckyyudhaprawira <luckyyudha15@gmail.com>
2020-06-04 15:29:14 +0800

    Penambahan String URL
    Penambahan String URL webhostapp

--- MenuMakanan/app/src/main/java/com/example/menumakanan/MainActivity.java
+++ MenuMakanan/app/src/main/java/com/example/menumakanan/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.menumakanan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -15,11 +25,49 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Menu> menus;
     int jumdata;
     private RequestQueue requestQueue;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.rv_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        menus=new ArrayList<>();
+        requestQueue= Volley.newRequestQueue(this);
+        parseJSON();
+    }
+    private void parseJSON() {
+        String url = "https://daftarmakan.000webhostapp.com/koneksi.php";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        jumdata = response.length();
+                        try {
+                            for (int i = 0; i < jumdata; i++) {
+                                JSONObject data = response.getJSONObject(i);
+                                String namaMenu = data.getString("nama");
+                                String hargaMenu = data.getString("harga");
+                                String gambarMenu = data.getString("gambar");
+                                String detailMenu = data.getString("keterangan");
+                                menus.add(new Menu(namaMenu, hargaMenu, gambarMenu, detailMenu));
+                            }
+                            menuAdapter = new MenuAdapter(MainActivity.this, menus);
+                            recyclerView.setAdapter(menuAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+        });
+        requestQueue.add(request);
     }
 }
