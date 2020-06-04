1b7801d80f0490b5cc1912743070be932dd6dde5
luckyyudhaprawira <luckyyudha15@gmail.com>
2020-06-04 15:13:18 +0800

    Penambahan definisi
    Penambahan definisi di mainactivity

--- MenuMakanan/app/src/main/java/com/example/menumakanan/MainActivity.java
+++ MenuMakanan/app/src/main/java/com/example/menumakanan/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.menumakanan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private MenuAdapter menuAdapter;
+    private RecyclerView recyclerView;
+    private ArrayList<Menu> menus;
+    int jumdata;
+    private RequestQueue requestQueue;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
