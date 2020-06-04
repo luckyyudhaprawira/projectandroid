36ce272c6034938bfc41c2524287351693e14731
luckyyudhaprawira <luckyyudha15@gmail.com>
2020-06-04 14:54:52 +0800

    Implement methods recycleview
    Implement methods recycleview

--- MenuMakanan/app/src/main/java/com/example/menumakanan/MenuAdapter.java
+++ MenuMakanan/app/src/main/java/com/example/menumakanan/MenuAdapter.java
@@ -2,6 +2,7 @@ package com.example.menumakanan;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +20,23 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder
         menus=menumakanan;
 
     }
+
+    @NonNull
+    @Override
+    public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MenuViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class MenuViewHolder extends RecyclerView.ViewHolder {
         public ImageView imdata;
         public TextView tvhargadata;
