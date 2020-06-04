39edfb00304724495ea3e322e7eb57195779eca6
luckyyudhaprawira <luckyyudha15@gmail.com>
2020-06-04 14:52:57 +0800

    Penambahan list
    Penambahan list menu makanan

--- MenuMakanan/app/src/main/java/com/example/menumakanan/MenuAdapter.java
+++ MenuMakanan/app/src/main/java/com/example/menumakanan/MenuAdapter.java
@@ -1,5 +1,6 @@
 package com.example.menumakanan;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,8 +8,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder> {
+import java.util.ArrayList;
 
+public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder> {
+    private Context context;
+    private ArrayList<Menu> menus;
+
+    public MenuAdapter (Context mcontext, ArrayList<Menu> menumakanan){
+        context=mcontext;
+        menus=menumakanan;
+
+    }
     public class MenuViewHolder extends RecyclerView.ViewHolder {
         public ImageView imdata;
         public TextView tvhargadata;
@@ -18,7 +28,7 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder
             imdata= itemView.findViewById(R.id.img_menu);
             tvhargadata= itemView.findViewById(R.id.tv_harga);
             tvnamadata= itemView.findViewById(R.id.tv_nama);
-            
+
 
 
 
