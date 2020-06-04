a35d522c5d9f1c439a9454bf301692814d7d0f8f
luckyyudhaprawira <luckyyudha15@gmail.com>
2020-06-04 14:46:22 +0800

    Construct menuviewholder
    Penambahan Imageview dan Textview

--- MenuMakanan/app/src/main/java/com/example/menumakanan/MenuAdapter.java
+++ MenuMakanan/app/src/main/java/com/example/menumakanan/MenuAdapter.java
@@ -1,13 +1,27 @@
 package com.example.menumakanan;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder> {
 
     public class MenuViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imdata;
+        public TextView tvhargadata;
+        public TextView tvnamadata;
+
+        public MenuViewHolder(@NonNull View itemView) {
+            imdata=
 
 
 
+
+            super(itemView);
+        }
     }
 
 }
