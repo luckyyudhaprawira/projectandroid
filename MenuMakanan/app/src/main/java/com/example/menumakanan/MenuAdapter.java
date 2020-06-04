3bb9853d051e80ac2f85baa2ce3b4d94e4fccbe9
luckyyudhaprawira <luckyyudha15@gmail.com>
2020-06-04 14:38:50 +0800

    Membuat Menu Adapter.java
    memasukkan recycleview dalam menu adapter

---
+++ MenuMakanan/app/src/main/java/com/example/menumakanan/MenuAdapter.java
@@ -0,0 +1,13 @@
+package com.example.menumakanan;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder> {
+
+    public class MenuViewHolder extends RecyclerView.ViewHolder {
+
+
+
+    }
+
+}
